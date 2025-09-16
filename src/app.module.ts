196b38ecd477c92c1f976e8cf1b955e3544cbbad
juzhiqiang <1020814597@qq.com>
2025-09-17 00:47:41 +0800

    feat： 添加静态文件读取mpa

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { StaticModule } from './static/static.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, StaticModule],
   controllers: [AppController],
   providers: [AppService],
 })
