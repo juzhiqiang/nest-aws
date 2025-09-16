a4fec18f0b53d4941a20f7f316ee834ff2c1ed85
juzhiqiang <1020814597@qq.com>
2025-09-16 22:36:00 +0800

    update: prisma add data

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 // src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
