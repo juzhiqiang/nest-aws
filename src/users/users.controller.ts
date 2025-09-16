845d71781f78c971006a498681658e5ca3063249
琚志强 <1020814597@qq.com>
2025-09-16 18:58:53 +0800

    update: update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller()
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get("users")
+  @Get()
   async getHello(): Promise<any> {
     return await this.usersService.findAll(1,10);
   }
