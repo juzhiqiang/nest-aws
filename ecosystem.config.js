62c51212d52e03aa2dfa25a3aa9f27b8ed98c98c
juzhiqiang <1020814597@qq.com>
2025-09-18 00:00:21 +0800

    update: 更新pm2 配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: 'nest-aws',
       script: 'dist/main.js',
-      instances: 'max', // 集群模式 = CPU 核心数
+      instances: '1', // 集群模式 = CPU 核心数
       exec_mode: 'cluster', // 负载均衡
       interpreter: './node_modules/.bin/ts-node', // 使用本地 ts-node
       env: {
@@ -16,6 +16,7 @@ module.exports = {
         NODE_ENV: 'production',
         TS_NODE_PROJECT: './tsconfig.json',
       },
+      monitor: false, // 生产关闭监控日志,windos 不关闭有错误提示
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       error_file: './logs/err.log',
       out_file: './logs/out.log',
